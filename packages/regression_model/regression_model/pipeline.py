5b37bc6eed5f2e7dfb0c4eee399bc1468f24a55c
GaTa-testing <GaTa_testing@protonmail.com>
2019-05-28 12:00:13 -0400

    Section 6.4 - Predictions(JLM)

--- packages/regression_model/regression_model/pipeline.py
+++ packages/regression_model/regression_model/pipeline.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import Lasso
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
 
-import preprocessors as pp
+from regression_model import preprocessors as pp
 
 # categorical variables with NA in the train set.
 CATERGORICAL_VARS_WITH_NA = [
@@ -30,24 +30,24 @@ CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
 price_pipe = Pipeline(
     [
         ('categorical_imputer',
-            pp.CategoricalImputer(variables=CATERGORICAL_VARS_WITH_NA)),
+            pp.CategoricalImputer(variables = CATERGORICAL_VARS_WITH_NA)),
         ('numerical_imputer',
-            pp.NumericalImputer(variables=NUMERICALS_VARS_WITH_NA)),
+            pp.NumericalImputer(variables = NUMERICALS_VARS_WITH_NA)),
         ('temporal_variable',
             pp.TemporalVariableEstimator(
                 variables = TEMPORAL_VARS,
-                reference_variable = TEMPORAL_VARS)),
+                reference_variable = DROP_FEATURES)),
         ('rare_label_encoder',
             pp.RareLabelCategoricalEncoder(
-                tol=0.01,
-                variables=CATEGORICAL_VARS)),
+                tol = 0.01,
+                variables = CATEGORICAL_VARS)),
         ('categorical_encoder',
-            pp.CategoricalEncoder(variables=CATEGORICAL_VARS)),
+            pp.CategoricalEncoder(variables = CATEGORICAL_VARS)),
         ('log_transformer',
-            pp.LogTransformer(variables=NUMERICALS_LOG_VARS)),
+            pp.LogTransformer(variables = NUMERICALS_LOG_VARS)),
         ('drop_features',
-            pp.DropUnecessaryFeatures(variables_to_drop=DROP_FEATURES)),
+            pp.DropUnecessaryFeatures(variables_to_drop = DROP_FEATURES)),
         ('scaler', MinMaxScaler()),
-        ('Linear_model', Lasso(alpha=0.005, random_state=422))
+        ('Linear_model', Lasso(alpha = 0.005, random_state = 422))
     ]
 )
