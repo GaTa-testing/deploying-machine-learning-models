db2560affcecd2f87df8bc5154a064959f13a533
GaTa-testing <GaTa_testing@protonmail.com>
2019-05-29 12:32:31 -0400

    Section 6.5 - Data Validation

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -18,3 +18,35 @@ FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood', 'OverallQual',
 			'1stFlrSF', 'GrLivArea', 'BsmtFullBath', 'KitchenQual',
 			'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageFinish',
 			'GarageCars', 'PavedDrive', 'LotFrontage', 'YrSold']
+
+# This variable is to calculate the temporal variable,
+# can be dropped afterwards
+DROP_FEATURES = 'YrSold'
+
+# Numerical variables with NA in train dataset
+NUMERICAL_VARS_WITH_NA = ['LotFrontage']
+
+# Categorical variables with NA in train dataset
+CATEGORICAL_VARS_WITH_NA = ['MasVnrType', 'BsmtQual', 'BsmtExposure',
+							 'FireplaceQu', 'GarageType', 'GarageFinish']
+
+TEMPORAL_VARS = 'YearRemodAdd'
+
+# Variables to log transform
+NUMERICAL_LOG_VARS = ['LotFrontage', '1stFlrSF', 'GrLivArea']
+
+# Categorical variables to encode.
+CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
+					'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
+					'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
+					'PavedDrive']
+
+NUMERICAL_NA_NOT_ALLOWED = [
+	feature for feature in FEATURES
+	if feature not in CATEGORICAL_VARS + NUMERICAL_VARS_WITH_NA
+]
+
+CATEGORICAL_NA_NOT_ALLOWED = [
+	feature for feature in CATEGORICAL_VARS
+	if feature not in CATEGORICAL_VARS_WITH_NA
+]
